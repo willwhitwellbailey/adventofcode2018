ae97ee46f5346b2407b5bdc71fa88ac5dfb19ac2
Will Whitwell Bailey <wwwhitwell@gmail.com>
2019-01-28 16:01:05 -0800

    following a new format since I sense a pattern

--- 20181201/frequencyFinder.go
+++ 20181201/frequencyFinder.go
@@ -2,23 +2,13 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"strconv"
 	"strings"
 )
 
-func getFileAsString(file string) string {
-	byteString, err := ioutil.ReadFile("input.txt")
-
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	return string(byteString)
-}
-
-func main() {
-	inputs := strings.Split(getFileAsString("input.txt"), "\n")
+func FrequencyFinder() {
+	fmt.Println("-----------------------------")
+	inputs := strings.Split(GetFileAsString("input.txt"), "\n")
 
 	var frequency int64 = 0
 	for i := 0; i < len(inputs); i++ {
@@ -33,5 +23,6 @@ func main() {
 		frequency += number
 	}
 
-	fmt.Println(frequency)
+	fmt.Println("Frequency Finder", frequency)
+	fmt.Println("-----------------------------")
 }
