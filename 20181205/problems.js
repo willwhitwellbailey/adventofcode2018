78119d6ef3f8d3c97b3714e79dff914ac3d2a0ee
Will Whitwell Bailey <wwwhitwell@gmail.com>
2019-02-12 11:10:37 -0800

    removing a single char

--- 20181205/problems.js
+++ 20181205/problems.js
@@ -25,6 +25,32 @@ function removeMatches(polymer) {
   return pass.length === polymer.length ? pass : removeMatches(pass);
 }
 
+function inPlace(polymer) {
+  for (let i = 0; i < polymer.length; i++) {
+    if (Math.abs(polymer.charCodeAt(i) - polymer.charCodeAt(i + 1)) === 32) {
+      polymer = polymer.slice(0, i) + polymer.slice(i + 2);
+      i -= 2;
+    }
+  }
+
+  return polymer;
+}
+
+function inPlaceWithoutChar(polymer, char) {
+  for (let i = 0; i < polymer.length; i++) {
+    if (polymer.charCodeAt(i) === char.charCodeAt(0) || polymer.charCodeAt(i) + 32 === char.charCodeAt(0)) {
+      polymer = polymer.slice(0, i) + polymer.slice(i + 1);
+      i--;
+    }
+    if (Math.abs(polymer.charCodeAt(i) - polymer.charCodeAt(i + 1)) === 32) {
+      polymer = polymer.slice(0, i) + polymer.slice(i + 2);
+      i -= 2;
+    }
+  }
+
+  return polymer;
+}
+
 function runTests() {
   let tests = [
     ['aA', ''],
@@ -39,7 +65,7 @@ function runTests() {
   
   tests.forEach(test => {
     console.log(`testing ${test[0]}:`);
-    let testResult = removeMatches(test[0]);
+    let testResult = inPlace(test[0]);
     console.log(`   resulted in '${testResult}'`);
     if (testResult !== test[1]) {
       console.error(`   ... '${test[0]}' did not produce expected outcome of '${test[1]}'`);
@@ -49,5 +75,51 @@ function runTests() {
   });
 }
 
-runTests();
-console.log(`\nsolution: polymers leftover - ${removeMatches(input).length}`);
+function runTestsWithoutChar() {
+  let tests = [
+    ['aA', 'b', ''],
+    ['Aa', 'a', ''],
+    ['aaAA', 'b', ''],
+    ['abAB', 'b', ''],
+    ['abcCBa', 'a', ''],
+    ['abcCBa', 'b', 'aa'],
+    ['ABbaC', 'a', 'C'],
+    ['ABbaC', 'c', ''],
+    ['dabAcCaCBAcCcaDA', 'a', 'dbCBcD'], // example from day 5
+    ['dabAcCaCBAcCcaDA', 'c', 'daDA'], // example from day 5
+    ['nNNnHhnNjyYLlLpPmMnHhNyYSwWovlLVbBEWwtTpPejJnNHhjJiIeLlEcCOhFfjuUeuUEJWVv', 'a', 'jLShW'] // snippet of input
+  ];
+  
+  tests.forEach(test => {
+    console.log(`testing ${test[0]}:`);
+    let testResult = inPlaceWithoutChar(test[0], test[1]);
+    console.log(`   resulted in '${testResult}'`);
+    if (testResult !== test[2]) {
+      console.error(`   ... '${test[0]}' did not produce expected outcome of '${test[2]}'`);
+    } else {
+      console.log(`   !!! '${test[0]}' produced expected outcome of '${test[2]}'`)
+    }
+  });
+}
+
+// runTests();
+// runTestsWithoutChar();
+// console.log(`\nsolution: polymers leftover (removeMatches) - ${removeMatches(input).length}`);
+// console.log(`\nsolution: polymers leftover (inPlace) - ${removeMatches(input).length}`);
+
+function getLetters() {
+  let letters = [];
+  for (let i = 'a'.charCodeAt(0); i <= 'z'.charCodeAt(0); i++) {
+    letters.push(String.fromCharCode(i));
+  }
+  return letters;
+}
+
+
+let minLenghth = getLetters().reduce((memo, letter) => {
+  let s = inPlaceWithoutChar(input, letter);
+  console.log(`...info: ${letter}--${s.length}`)
+  return Math.min(memo, s.length);
+}, input.length);
+
+console.log(`part 2 solution: ${minLenghth}`);
